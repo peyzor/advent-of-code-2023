e9166acbb69fce43ad14ee3b856555450639db9d
Peyman <hassani.peyman627@gmail.com>
2024-04-09 16:14:17 +0330

    Update day 4 part 2 to be a lot faster

--- day04/part2.py
+++ day04/part2.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Card:
     def __init__(self, id, win_nums, lose_nums):
         self.id = id
@@ -9,6 +12,7 @@ class Card:
 
 
 CARDS = []
+CARDS_COUNT = defaultdict(int)
 
 
 def create_card(line):
@@ -26,32 +30,26 @@ def create_card(line):
     return card
 
 
-def stock_count(card_id):
-    count = 0
-    for card in CARDS:
-        if card.id == card_id:
-            count += 1
-
-    return count
-
-
 def main():
     original_cards = []
     with open('input.txt') as f:
         for line in f:
             card = create_card(line)
             CARDS.append(card)
+            CARDS_COUNT[card.id] += 1
             original_cards.append(card)
 
     for i in range(len(CARDS)):
         card = CARDS[i]
         matched_nums = set(card.win_nums) & set(card.lose_nums)
-        times = stock_count(card.id)
+        times = CARDS_COUNT[card.id]
 
         c = 1 if len(matched_nums) > 0 else 0
         while matched_nums and c <= len(matched_nums) and i + c < len(original_cards):
             for _ in range(times):
-                CARDS.append(original_cards[i + c])
+                won_card = original_cards[i + c]
+                CARDS.append(won_card)
+                CARDS_COUNT[won_card.id] += 1
 
             c += 1
 
