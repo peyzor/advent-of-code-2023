8ed80f13abd4983384dbc422db3e2c832944f7d1
Peyman <hassani.peyman627@gmail.com>
2024-04-09 16:02:18 +0330

    Add day 4 part 2

---
+++ day04/part2.py
@@ -0,0 +1,62 @@
+class Card:
+    def __init__(self, id, win_nums, lose_nums):
+        self.id = id
+        self.win_nums = win_nums
+        self.lose_nums = lose_nums
+
+    def __repr__(self):
+        return f'#{self.id}'
+
+
+CARDS = []
+
+
+def create_card(line):
+    card_id_part, numbers_part = line.split(':')
+    card_id = int(card_id_part[5:])
+
+    win_nums_part, lose_nums_part = numbers_part.split('|')
+    lose_nums = [int(n) for n in lose_nums_part.split()]
+    win_nums = [int(n) for n in win_nums_part.split()]
+    card = Card(
+        id=card_id,
+        lose_nums=lose_nums,
+        win_nums=win_nums
+    )
+    return card
+
+
+def stock_count(card_id):
+    count = 0
+    for card in CARDS:
+        if card.id == card_id:
+            count += 1
+
+    return count
+
+
+def main():
+    original_cards = []
+    with open('input.txt') as f:
+        for line in f:
+            card = create_card(line)
+            CARDS.append(card)
+            original_cards.append(card)
+
+    for i in range(len(CARDS)):
+        card = CARDS[i]
+        matched_nums = set(card.win_nums) & set(card.lose_nums)
+        times = stock_count(card.id)
+
+        c = 1 if len(matched_nums) > 0 else 0
+        while matched_nums and c <= len(matched_nums) and i + c < len(original_cards):
+            for _ in range(times):
+                CARDS.append(original_cards[i + c])
+
+            c += 1
+
+    print(len(CARDS))
+
+
+if __name__ == '__main__':
+    main()
